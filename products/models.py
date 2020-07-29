d200c38fdd9e853bbdc705af0f876ee79a3906c2
Alen Krga <alen.krga1@gmail.com>
2020-07-29 13:21:53 +0200

    Products filter by category

--- products/models.py
+++ products/models.py
@@ -1,3 +1,4 @@
+from freelancesolution.enums import ProductType
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -6,6 +7,7 @@ class Product(models.Model):
 	name = models.CharField(max_length = 200)
 	description = models.TextField()
 	price = models.IntegerField(default = 0)
+	product_type = models.CharField(max_length=15, choices=[(tag.name, tag.value) for tag in ProductType], null = True, blank = True)
 	img = models.ImageField(upload_to = 'products')
 	high_res = models.ImageField(upload_to = 'products', null = True, blank = True)
 
--- products/views.py
+++ products/views.py
@@ -1,14 +1,13 @@
+from freelancesolution.enums import ProductType
 from django.views.generic import ListView
 from django.shortcuts import render
 from .models import Product
 
 def products(request):
-	search_param = request.GET.get('search')
+	search_param = request.GET.get('search', '')
+	category_param = request.GET.get('category', '')
 
-	if search_param:
-		products = Product.objects.filter(name__icontains = search_param)
-	else:
-		products = Product.objects.all()
+	products = Product.objects.filter(name__icontains = search_param, product_type__icontains = category_param)
 
 	return render(request, 'products.html', {'products': products})
 
